54254e0e40541ead5233748f03a5827a17c77345
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2018-04-02 20:49:11 -0700

    add draft of functions for identifying prime numbers

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+
+function generateRange(number) {
+  range = [];
+  for (var i=0; i<=number; i++) {
+    range.push(i);
+  }
+  return range;
+}
+
+function excludeNonPrime(range) {
+  for (var j=2; j<=range.length; j++) {
+    for (var p=2; p<range.length; p++) {
+      if ((j*p) === range[j]) {
+        var newRange = range.slice
+      }
+    }
+    if ((j*p) ===
+
+    range.forEach(function(number)) {
+      var count = 1;
+      while (count<range.length) {
+        (count*p) === number;
+
+      }
+    }
+  }
+}
