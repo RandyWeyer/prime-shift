3d7a421fd3d53cc622f8b7da73cdb5e658e05969
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2018-04-04 20:54:57 -0700

    add function to exclude non-prime numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,53 @@
-
+// var newRange = [];
 function generateRange(number) {
   range = [];
-  for (var i=0; i<=number; i++) {
+  for (var i=2; i<=number; i++) {
     range.push(i);
   }
   return range;
 }
 
 function excludeNonPrime(range) {
-  var newRange = [];
-  for (var j=2; j<=range.length; j++) {
-    for (var p=0; p<j; p++) {
-      if ((j%p) !== 0) {
-        newRange.push(range[j]);
-      }
-    }
+  range.forEach(function(element) {
+    var newRange = [1];
+    if (element === 0 || element === 1) {
+            return false;
+        }
+        for (var i = 2; i < element; i++) {
+            if (element%i === 0) {
+                return false;
+            }
+        }
+        return true;
+        newRange.push(element);
+    });
   }
-  return newRange;
-}
+//     var index = 0;
+//     var i = 2;
+//     if (element>2 && element <=range.length) {
+//       while (i<element) {
+//         if (element%i ===0) {
+//           index = range.indexOf(element);
+//           range.splice(index,1);
+//           i++;
+//         } else {}
+//       }
+//     }
+//     });
+//   return range;
+// }
+  //
+  // for (var j=0; j<=range.length; j++) {
+  //   for (var p=2; p<range[j]; p++) {
+  //     if ((range[j]%p) === 0) {
+  //       break;
+  //     } else if ((range[j]%p) !== 0) {
+  //       newRange.push(range[j]);
+  //
+  //     }
+  //   }
+  // }
+  // return newRange;
 
 $(document).ready(function() {
   $("form#prime").submit(function(event) {
